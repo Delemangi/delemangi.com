9eaa82af748dd243339dc5a3c8cf79aca4bdb368
Delemangi <milev.stefan@gmail.com>
2023-06-29 06:14:04 +0200

    Update Discord tag

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import { Tooltip } from "bootstrap";
 import "../scss/styles.scss";
 
 async function click() {
-  await navigator.clipboard.writeText("stefan#4595");
+  await navigator.clipboard.writeText("delemangi");
   tooltip.show();
   setTimeout(() => tooltip.hide(), 1000);
 }
