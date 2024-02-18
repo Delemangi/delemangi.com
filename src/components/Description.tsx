3f806e77ccd0cd764952f8a28d11f311d9ec09b6
Delemangi <milev.stefan@gmail.com>
2024-02-18 17:51:16 +0100

    Richer description

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -5,7 +5,7 @@ const Description = () => {
   return (
     <DescriptionContainer>
       <Typography align="center">
-        I mostly make web apps and Discord bots. I also occasionally play some games.
+        I mostly make web apps, Discord bots and scrapers. I also occasionally play some video games, mostly those within the survival or factory genres.
       </Typography>
     </DescriptionContainer>
   );
