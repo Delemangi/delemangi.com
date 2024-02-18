9292b010d229d43be6f7a5ed638ea81e37864b86
Delemangi <milev.stefan@gmail.com>
2024-02-18 16:06:08 +0100

    Add branding

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,11 +1,11 @@
-import { Typography } from "@mui/material";
+import { Link, Typography } from "@mui/material";
 import Column from "./Column";
 
 const Intro = () => {
   return (
     <Column sx={{ animation: "fadeIn 1.5s ease-in-out" }}>
       <Typography fontSize={48}>Delemangi</Typography>
-      <Typography fontSize={20}>Software Engineer</Typography>
+      <Typography fontSize={16}>Software Engineer @ <Link href="https://codechem.com">CodeChem</Link></Typography>
     </Column>
   )
 };
