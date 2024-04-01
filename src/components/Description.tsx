b5e6fb95a49b1be5f21b65cf2358e47437036e73
Delemangi <milev.stefan@gmail.com>
2024-04-01 14:08:28 +0200

    Update description

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -5,9 +5,10 @@ const Description = () => {
   return (
     <DescriptionContainer>
       <Typography align="center">
-        In general, I mostly make different types of web apps and Discord bots
-        in my free time, as well as occasionally playing some video games,
-        mostly indie. You may check my social media links above.
+        Welcome to my space. I go by the nickname Delemangi, and in general, I
+        mostly make different types of web apps and Discord bots in my free
+        time, as well as occasionally playing some video games, mostly indie.
+        You may check my social media links above.
       </Typography>
     </DescriptionContainer>
   );
