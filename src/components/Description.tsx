596a158cd3a8aa8cf29ee09f5c2ef18c484d9ff6
Delemangi <milev.stefan@gmail.com>
2024-04-01 18:13:27 +0200

    Update description

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,4 +1,5 @@
 import DescriptionContainer from './DescriptionContainer';
+import Discord from './Discord';
 import { Typography } from '@mui/material';
 
 const Description = () => {
@@ -7,8 +8,8 @@ const Description = () => {
       <Typography align="center">
         Welcome to my space. I go by the nickname Delemangi, and in general, I
         mostly make different types of web apps and Discord bots in my free
-        time, as well as occasionally playing some video games, mostly indie.
-        You may check my social media links above.
+        time, as well as occasionally playing some video games, mostly indie. I
+        also run these Discord servers: <Discord />.
       </Typography>
     </DescriptionContainer>
   );
--- src/components/Discord.tsx
+++ src/components/Discord.tsx
@@ -1,37 +1,28 @@
 import UnderlinedLink from './UnderlinedLink';
-import { Typography } from '@mui/material';
 
 const Discord = () => {
   return (
     <>
-      <Typography fontSize={16}>Leading several Discord servers:</Typography>
-      <Typography fontSize={16}>
-        <UnderlinedLink
-          href="https://discord.gg/finki-studenti-810997107376914444"
-          target="_blank"
-        >
-          FCSE Students
-        </UnderlinedLink>
-      </Typography>
-      <Typography fontSize={16}>
-        <UnderlinedLink
-          href="https://discord.gg/wZMqxC7AY6"
-          target="_blank"
-        >
-          UKIM Students
-        </UnderlinedLink>
-      </Typography>
-      <Typography
-        fontSize={16}
-        marginBottom={2}
+      <UnderlinedLink
+        href="https://discord.gg/finki-studenti-810997107376914444"
+        target="_blank"
       >
-        <UnderlinedLink
-          href="https://discord.gg/K2ND2ykP2j"
-          target="_blank"
-        >
-          42.mk
-        </UnderlinedLink>
-      </Typography>
+        FCSE Students
+      </UnderlinedLink>
+      ,{' '}
+      <UnderlinedLink
+        href="https://discord.gg/wZMqxC7AY6"
+        target="_blank"
+      >
+        UKIM Students
+      </UnderlinedLink>
+      ,{' '}
+      <UnderlinedLink
+        href="https://discord.gg/K2ND2ykP2j"
+        target="_blank"
+      >
+        42.mk
+      </UnderlinedLink>
     </>
   );
 };
